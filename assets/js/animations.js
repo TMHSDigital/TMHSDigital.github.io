3dff881230d48ec367fcea35fdf68a49cc00393d
fOuttaMyPaint <TMhospitalitystrategies@gmail.com>
2024-08-23 14:23:21 -0400

    add terminal animations

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -1,58 +1,43 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Glitch Effect on Title
-    gsap.to(".glitch", {
-        duration: 1,
-        text: {
-            value: "Webfusion Solutions",
-            newClass: "glitch",
-            oldClass: "glitch",
-            delimiter: ""
-        },
-        repeat: -1,
-        repeatDelay: 0.2,
-        ease: "power2.inOut"
-    });
-
-    // Scan line effect on body
-    gsap.to("body::before", {
-        duration: 1.5,
-        backgroundPosition: "0 100%",
-        ease: "none",
-        repeat: -1
-    });
-
-    // Fade in and slide up effect for sections
-    gsap.from(".container", {
-        duration: 1.5,
-        y: 100,
-        opacity: 0,
-        stagger: 0.2,
-        ease: "power2.out",
-        scrollTrigger: {
-            trigger: ".container",
-            start: "top 80%",
-            end: "bottom 60%",
-            toggleActions: "play none none reverse",
-            markers: true
-        }
-    });
-
-    // Highlight effect on hover for links
-    gsap.utils.toArray("a").forEach(link => {
-        link.addEventListener("mouseenter", () => {
-            gsap.to(link, {
-                duration: 0.3,
-                textShadow: "0px 0px 10px #0F0",
-                ease: "power1.inOut"
-            });
+    // Simulate typing effect
+    const texts = document.querySelectorAll(".container h1, .container h2, .container p");
+    texts.forEach(text => {
+        const chars = text.innerHTML.split("");
+        text.innerHTML = "";
+        chars.forEach((char, i) => {
+            setTimeout(() => {
+                text.innerHTML += char;
+            }, 100 * i);
         });
+    });
 
-        link.addEventListener("mouseleave", () => {
-            gsap.to(link, {
-                duration: 0.3,
-                textShadow: "0px 0px 5px #00FF41",
-                ease: "power1.inOut"
-            });
+    // Blinking cursor effect
+    const cursor = document.createElement("span");
+    cursor.className = "cursor";
+    cursor.innerHTML = "|";
+    document.body.appendChild(cursor);
+
+    gsap.to(".cursor", {
+        opacity: 0,
+        repeat: -1,
+        yoyo: true,
+        ease: "power1.inOut",
+        duration: 0.6
+    });
+
+    // Scroll-triggered section fade-in
+    gsap.utils.toArray(".container").forEach(section => {
+        gsap.from(section, {
+            duration: 1.5,
+            opacity: 0,
+            y: 50,
+            ease: "power2.out",
+            scrollTrigger: {
+                trigger: section,
+                start: "top 80%",
+                end: "bottom 60%",
+                toggleActions: "play none none reverse"
+            }
         });
     });
 });
